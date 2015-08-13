57cdc56be916ccd26302a8a353d6e0aec6d26572
Tom Keffer <tkeffer@gmail.com>
2015-08-13 16:55:34 -0700

    Sends new packet to client

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
@@ -9,6 +10,9 @@ var port = process.env.PORT || 3000;
 // See http://expressjs.com/4x/api.html#app.set
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
@@ -24,10 +28,52 @@ app.get('/', function (req, res, next) {
 //  res.sendfile(__dirname + '/index.html');
 //});
 
+var ws_socket;
 io.on('connection', function (socket) {
     console.log("got a connection");
+    ws_socket = socket;
     socket.emit('news', {hello: 'world'});
     socket.on('my other event', function (data) {
         console.log(data);
     });
 });
+
+
+
+var mongo = require('mongodb');
+var bodyParser = require('body-parser');
+
+var MongoClient = mongo.MongoClient;
+var mongo_url = "mongodb://localhost:27017/wee_node";
+
+var loop_packets;
+MongoClient.connect(mongo_url, function (err, db) {
+    if (err) throw err;
+    // Create a capped collection if it doesn't exist already
+    db.listCollections({name:'loop_packets'}).toArray(function(err,names){
+        if (names.length){
+            console.log("Collection loop_packets already exists");
+        } else {
+            db.createCollection("loop_packets", {capped:true, size:1000000, max:1800});
+            console.log("Created collection loop_packets");
+        }
+    });
+    db.collection('loop_packets', function (err, collection) {
+        loop_packets = collection;
+    })
+});
+
+// RESTful interface for storing loop packets into MongoDB
+app.post('/api/loop', function (req, res) {
+    // Get the packet out of the request body:
+    var packet = req.body.packet;
+    console.log("Got packet", packet);
+    // Insert it into the database
+    loop_packets.insert(packet, function (err, result) {
+        if (err) throw err;
+    });
+    res.send("SUCCESS");
+    ws_socket.emit("packet", packet);
+    console.log("Sent packet");
+});
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+console.log("connecting...");
+var socket = io('http://localhost:3000');
+socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', {my: 'data'});
+});
