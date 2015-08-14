872f4115a7dd1d20554605d827dda83e41743742
Tom Keffer <tkeffer@gmail.com>
2015-08-14 09:39:36 -0700

    Rearranged things. Successfully handles multiple clients.

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 var pubsub = require('./pubsub');
-var mongo = require('mongodb');
-var bodyParser = require('body-parser');
-var path = require('path');
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
 var port = process.env.PORT || 3000;
 
-// view engine setup. Set some values in the app settings table.
-// See http://expressjs.com/4x/api.html#app.set
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
 
+var io = require('socket.io')(server);
+
+// view engine setup. Set some values in the app settings table.
+// See http://expressjs.com/4x/api.html#app.set
+var path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+/*
+ * Set up express routes
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Express'
+        title: 'Express',
+        server: 'http://nuc:3000'
     });
+});
+
+// RESTful interface for storing loop packets into MongoDB
+app.post('/api/loop', function (req, res) {
+    // Get the packet out of the request body:
+    var packet = req.body.packet;
+    // Insert it into the database
+    loop_packets.insert(packet, function (err, result) {
+        if (err) throw err;
+    });
+    res.send("SUCCESS");
+    pubsub.publish('new_packet', packet);
 });
 
 //app.get('/', function (req, res) {
 //  res.sendfile(__dirname + '/index.html');
 //});
 
-var ws_socket;
-
-io.on('connection', function (socket) {
-    console.log("got a connection");
-
-    // subscribe to any new packets
-    pubsub.subscribe('new_packet', function(packet){
-        console.log("ready to emit packet", packet);
-        socket.emit('packet', packet);
-    });
-    socket.emit('news', {hello: 'world'});
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
-});
-
-
+/*
+ * Set up connection to MongoDB
+ */
+var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
 var mongo_url = "mongodb://localhost:27017/wee_node";
 
@@ -68,17 +73,22 @@ MongoClient.connect(mongo_url, function (err, db) {
     })
 });
 
-// RESTful interface for storing loop packets into MongoDB
-app.post('/api/loop', function (req, res) {
-    // Get the packet out of the request body:
-    var packet = req.body.packet;
-    console.log("Got packet", packet);
-    // Insert it into the database
-    loop_packets.insert(packet, function (err, result) {
-        if (err) throw err;
+/*
+ * Set up WebSocket connection to any interested clients.
+ */
+
+io.on('connection', function (socket) {
+    console.log("got a connection");
+
+    // subscribe to any new packets
+    pubsub.subscribe('new_packet', function(packet){
+        console.log("ready to emit packet", packet);
+        socket.emit('packet', packet);
     });
-    res.send("SUCCESS");
-    pubsub.publish('new_packet', packet);
-    console.log("Sent packet");
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
+
 
