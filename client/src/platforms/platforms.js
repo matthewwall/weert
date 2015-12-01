f66ac4a7a7911242f36642e2586297728a7accdb
Tom Keffer <tkeffer@gmail.com>
2015-12-01 10:04:09 -0800

    Got platforms view working

--- client/src/platforms/platforms.js
+++ client/src/platforms/platforms.js
@@ -1,17 +1,26 @@
+"use strict";
+
 angular
     .module('platforms', ['ngResource'])
 
     .controller('PlatformListCtrl', ['$scope', 'Platform',
         function ($scope, Platform) {
-            $scope.platforms = Platform.getall();
-            //$scope.orderProp = 'age';
+
+            Platform.getAllByValue().$promise.then(function (results) {
+                $scope.platforms = results;
+                // For each property, add a property 'link' that points to the property detail
+                $scope.platforms.forEach(function (platform) {
+                    platform.link = 'api/v1/platforms/' + platform._id;
+                });
+            });
+            $scope.orderProp = '_id';
         }])
 
     .factory('Platform', ['$resource',
         function ($resource) {
             var result = $resource('api/v1/platforms', {}, {
-                getall: {method: 'GET', isArray: true},
-                query : {method: 'GET', params: {phoneId: 'phones', foo: 'bar'}, isArray: true}
+                getAllByValue: {method: 'GET', params: {as: 'values'}, isArray: true},
+                query        : {method: 'GET', params: {phoneId: 'phones', foo: 'bar'}, isArray: true}
             });
             return result;
         }]);
