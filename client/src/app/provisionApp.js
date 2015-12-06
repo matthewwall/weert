87fc6601971c13713f617b90dce973ff00413254
Tom Keffer <tkeffer@gmail.com>
2015-12-05 17:05:48 -0800

    Refactored client into separate templates, controlled by tabs

--- client/src/app/provisionApp.js
+++ client/src/app/provisionApp.js
@@ -3,11 +3,14 @@
 /* App Module */
 
 angular
-    .module('provisionApp', [
-        'ngRoute',
-        'xeditable',
-        'platform'
-    ])
+    .module('provisionApp',
+        [
+            'ngRoute',
+            'xeditable',
+            'ngAnimate',
+            'ui.bootstrap',
+            'platform'
+        ])
 
     .run(function (editableOptions) {
         editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
@@ -15,14 +18,33 @@ angular
 
     .config(['$routeProvider',
         function ($routeProvider) {
+
             $routeProvider
                 .when('/platforms', {
-                    templateUrl: 'src/platform/platforms-list.html',
+                    templateUrl: 'src/platform/platform-list.html',
                     controller : 'PlatformListCtrl'
                 })
-
                 .otherwise({
                     redirectTo: '/platforms'
                 })
             ;
-        }]);
+        }])
+
+    .controller('TabsCtrl', ['$scope', function ($scope) {
+        $scope.tabs = [
+            {
+                label: 'Details',
+                url  : "src/platform/platform-detail.tpl.html"
+            }, {
+                label: 'Location',
+                url  : "src/location/location.tpl.html"
+            }, {
+                label: 'Streams',
+                url  : "src/stream/stream.tpl.html"
+            }];
+
+        $scope.setActiveTab = function (tab) {
+            $scope.activeTab = tab.url;
+        };
+        $scope.setActiveTab($scope.tabs[0]);
+    }]);
